0c1810cfa7177ee3b3235d22d95e32e95a7bcf7a
楊崇文 <59813155+kevin19680629@users.noreply.github.com>
2024-03-09 16:17:35 +0800

    Update lesson6_3.sql

--- sql/lesson6_3.sql
+++ sql/lesson6_3.sql
@@ -26,3 +26,22 @@ SELECT first_name || ' ' || last_name AS full_name,
 FROM payment LEFT JOIN customer USING (customer_id)
 GROUP BY full_name
 ORDER BY 總合 DESC;
+
+SELECT first_name || ' ' || last_name AS full_name,
+	   SUM(amount) AS 總合
+FROM payment LEFT JOIN customer USING (customer_id)
+GROUP BY full_name
+HAVING SUM(amount) >= 200
+ORDER BY 總合 DESC;
+
+/* ---   */
+SELECT 名稱, SUM(進站人數) AS 總進站人數
+FROM gate_count LEFT JOIN stations ON id=編號
+GROUP BY 名稱
+ORDER BY 總進站人數 DESC;
+
+SELECT 名稱, SUM(進站人數) AS 總進站人數
+FROM gate_count LEFT JOIN stations ON id=編號
+GROUP BY 名稱
+HAVING SUM(進站人數) >= 200
+ORDER BY SUM(進站人數) DESC;
