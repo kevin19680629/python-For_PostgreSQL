778e90068f96a5797dd267f991c6311b1541babf
楊崇文 <59813155+kevin19680629@users.noreply.github.com>
2024-03-09 16:27:53 +0800

    Update lesson6_2.sql

--- sql/lesson6_2.sql
+++ sql/lesson6_2.sql
@@ -41,3 +41,15 @@ SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
 ORDER BY 進站人數 DESC
 LIMIT 10;
+
+/* ---   */
+SELECT 名稱, SUM(進站人數) AS 總進站人數
+FROM gate_count LEFT JOIN stations ON id=編號
+GROUP BY 名稱
+ORDER BY 總進站人數 DESC;
+
+SELECT 名稱, SUM(進站人數) AS 總進站人數
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+GROUP BY 名稱
+HAVING SUM(進站人數) >= 5000000
+ORDER BY SUM(進站人數) DESC;
