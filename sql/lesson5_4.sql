8f33309f7e66b04a2015a7d95ef15f749d19bcb6
楊崇文 <59813155+kevin19680629@users.noreply.github.com>
2024-03-09 11:29:09 +0800

    Update lesson5_4.sql

--- sql/lesson5_4.sql
+++ sql/lesson5_4.sql
@@ -2,7 +2,6 @@ CREATE TABLE basket_a(
 	a INT PRIMARY KEY,
 	fruit_a VARCHAR(100) NOT NULL
 );
-
 CREATE TABLE basket_b(
 	b INT PRIMARY KEY,
 	fruit_b VARCHAR(100) NOT NULL
@@ -14,7 +13,6 @@ VALUES
     (2, 'Orange'),
     (3, 'Banana'),
     (4, 'Cucumber');
-
 INSERT INTO basket_b (b, fruit_b)
 VALUES
     (1, 'Orange'),
@@ -25,7 +23,39 @@ VALUES
 SELECT * FROM basket_a;
 SELECT * FROM basket_b;
 
-
 /*inner join*/
 SELECT a,fruit_a,b,fruit_b
 FROM basket_a INNER JOIN basket_b ON fruit_a = fruit_b;
+
+/*left join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a LEFT JOIN basket_b ON fruit_a = fruit_b;
+
+/*left join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a LEFT JOIN basket_b ON fruit_a = fruit_b
+WHERE b IS NULL;
+
+/*right join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a RIGHT JOIN basket_b ON fruit_a = fruit_b;
+
+/*right join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a RIGHT JOIN basket_b ON fruit_a = fruit_b
+WHERE a IS NULL;
+
+/*full outer join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b
+WHERE a IS NOT NULL AND b IS NOT NULL;
+
+/*full outer join*/
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b
+WHERE a IS NOT NULL OR b IS NOT NULL;
+
+/* full outer join with where */
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b	
+WHERE a IS NULL OR b IS NULL;
