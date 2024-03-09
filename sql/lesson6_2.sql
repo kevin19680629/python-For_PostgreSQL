3b0f543a4be74fa06e53f9dc1144df9debf4350b
楊崇文 <59813155+kevin19680629@users.noreply.github.com>
2024-03-09 15:11:37 +0800

    aaa

--- sql/lesson6_2.sql
+++ sql/lesson6_2.sql
@@ -1,3 +1,4 @@
+SELECT * FROM stations;
 SELECT * FROM gate_count;
 
 SELECT *
@@ -22,4 +23,9 @@ WHERE 名稱 = '基隆' AND 日期 = '2022-03-01';
 SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
 WHERE 名稱 = '基隆' AND (日期 BETWEEN '2022-03-01' AND '2022-03-31')
+ORDER BY 日期 ASC;
+
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count, stations
+WHERE 站點編號=編號 AND 名稱 = '基隆' AND (日期 BETWEEN '2022-03-01' AND '2022-03-31')
 ORDER BY 日期 ASC;
