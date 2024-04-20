3c272b50346691c6b0e7f770b34998b5e9040fdc
楊崇文 <59813155+kevin19680629@users.noreply.github.com>
2024-04-20 01:08:40 +0000

    2024-04-20

---
+++ lession14/lession14_1.py
@@ -0,0 +1,14 @@
+# requirements.txt
+# psycopg2-binary
+# python-dotenv
+# streamlit
+# 
+# pip install -r requirements.txt
+# streamlit run lession14_1.py
+import streamlit as st
+
+st.write('Hello, *World!* :sunglasses:')
+st.markdown('## 這是標題!')
+st.title("我是title!")
+st.header("我是header")
+st.subheader("我是sub header")
---
+++ lession14/lession14_3.py
@@ -0,0 +1,8 @@
+# 連接埠 --> http://127.0.0.1:8501/
+import streamlit as st
+
+with st.sidebar:
+    st.write("# 主題：")
+    st.title("我是title")
+    st.header("我是header")
+    st.subheader("我是subHeader")
